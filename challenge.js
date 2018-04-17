3fc864c38d5f97cc34fd6c6b72130d7e78bf1b95
Pedro Silva <pedro.silva@glintt.com>
2018-04-17 17:58:21 +0100

    Refactoring

--- challenge.js
+++ challenge.js
@@ -1,32 +1,12 @@
-
-const matrix = [
-	[0, 0, 0, 0, 1, 1],
-	[0, 0, 0, 0, 1, 1],
-	[0, 0, 0, 0, 0, 1],
-	[0, 1, 0, 0, 1, 1],
-	[0, 1, 1, 0, 0, 0],
-	[0, 0, 0, 0, 1, 1]
-]
-const len = matrix[0].length
-
-var visits = []
-
-for (i = 0; i < len; i++) {
-  visits[i] = [];
-  for (j = 0; j < len; j++) {
-    visits[i][j] = 0;
-  }
-}
-const points = []
-
+// Check surroundings
 const check_surr = (i, j, p) => {
   if (matrix[i][j] == 1 && !visits[i][j]) {
     visits[i][j] = 1
     p.push([i, j])
-    if(i < len - 1) {
+    if (i < len - 1) {
       check_surr(i + 1, j, p)
     }
-    if(i > 0) {
+    if (i > 0) {
       check_surr(i - 1, j, p)
     }
     if (j < len - 1) {
@@ -38,13 +18,39 @@ const check_surr = (i, j, p) => {
   }
 }
 
-for(var i = 0; i < len; i++) {
-	for(var j = 0; j < len; j++) {
-		if(matrix[i][j]  && visits[i][j] == 0) {
-      var p = []
-			check_surr(i, j, p)
-			points.push(p)
-		}
-	}
+const matrix = [
+	[0, 0, 0, 0, 1, 1],
+	[0, 0, 0, 0, 1, 1],
+	[0, 0, 0, 0, 0, 1],
+	[0, 1, 0, 0, 1, 1],
+	[0, 1, 1, 0, 0, 0],
+	[0, 0, 0, 0, 1, 1]
+]
+const len = matrix[0].length
+
+// Create visits matrix
+let visits = []
+
+for (i = 0; i < len; i++) {
+  visits[i] = [];
+  for (j = 0; j < len; j++) {
+    visits[i][j] = 0;
+  }
 }
-console.log(points)
+
+
+
+const groups = []
+
+for(let i = 0; i < len; i++) {
+  for(let j = 0; j < len; j++) {
+    if(matrix[i][j]  && visits[i][j] == 0) {
+      let group = []
+      check_surr(i, j, group)
+      groups.push(group)
+    }
+  }
+}
+
+
+console.log(groups)
