8403ff1bcb2793684c1306c3011e4326c44c6690
PedroS11 <pedrosilva1137@gmail.com>
2018-04-17 23:22:57 +0100

    Revert "new version with more files"
    This reverts commit 0eed52f8ca3b9f68ad9a611274c2df8aeb137a66.

--- challenge.js
+++ challenge.js
@@ -1,66 +1,54 @@
 // Check surroundings
-const check_surroundings = (y, x, p) => {
-	if (matrix[y][x] && !visits[y][x]) {
-		visits[y][x] = 1
-		p.push([y, x])
-		if (y < height - 1) {
-		  	check_surroundings(y + 1, x, p)
-		}
-		if (y > 0) {
-		  	check_surroundings(y - 1, x, p)
-		}
-		if (x < width - 1) {
-		  	check_surroundings(y, x + 1, p)
-		}
-		if (x > 0) {
-		  	check_surroundings(y, x - 1, p)
-		}
-	}
+const check_surr = (i, j, p) => {
+  if (matrix[i][j] == 1 && !visits[i][j]) {
+    visits[i][j] = 1
+    p.push([i, j])
+    if (i < len - 1) {
+      check_surr(i + 1, j, p)
+    }
+    if (i > 0) {
+      check_surr(i - 1, j, p)
+    }
+    if (j < len - 1) {
+      check_surr(i, j + 1, p)
+    }
+    if (j > 0) {
+      check_surr(i, j - 1, p)
+    }
+  }
+}
+
+const matrix = [
+	[0, 0, 0, 0, 1, 1],
+	[0, 0, 0, 0, 1, 1],
+	[0, 0, 0, 0, 0, 1],
+	[0, 1, 0, 0, 1, 1],
+	[0, 1, 1, 0, 0, 0],
+	[0, 0, 0, 0, 1, 1]
+]
+const len = matrix[0].length
+
+// Create visits matrix
+let visits = []
+
+for (i = 0; i < len; i++) {
+  visits[i] = [];
+  for (j = 0; j < len; j++) {
+    visits[i][j] = 0;
+  }
 }
 
 // Get groups
-const find_groups = () => {
-	const result = []
-	for(let y = 0; y < height; y++) {
-		for(let x = 0; x < width; x++) {
-			if (matrix[y][x] && !visits[y][x]) {
-				let group = []
-				check_surroundings(y, x, group)
-				if(group.length > 1)
-					result.push(group)
-			}
-		}
-	}
-	return result
+const groups = []
+
+for(let i = 0; i < len; i++) {
+  for(let j = 0; j < len; j++) {
+      let group = []
+      check_surr(i, j, group)
+      if(group.length)
+        groups.push(group)
+  }
 }
 
-// Create visits matrix
-const create_visits = () => {
-	const visits = []
 
-	for (y = 0; y < height; y++) {
-		visits[y] = [];
-		for (x = 0; x < width; x++) {
-		  visits[y][x] = 0;
-		}
-	}
-	return visits
-}
-
-const fs = require('fs');
-
-if (process.argv.length !== 3) {
-    console.log("USAGE: node index.js FILE_NAME");
-    return;
-}
-
-const fileName = process.argv[2];
-const matrix = JSON.parse(fs.readFileSync(fileName));
-
-const width = matrix[0].length
-const height = matrix.length
-
-// Create visits matrix
-const visits = create_visits()
-const result = find_groups()
-
+console.log(groups)
